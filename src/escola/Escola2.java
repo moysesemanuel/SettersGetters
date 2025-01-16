707c20f1fffc44d58542bc9c48baae5357c8d312
Moyses Emanuel <164270978+moysesemanuel@users.noreply.github.com>
2025-01-16 17:12:32 -0300

    primeiro commit pelo inteliJ

---
+++ src/escola/Escola2.java
@@ -0,0 +1,13 @@
+package escola;
+
+public class Escola2 {
+    public static void main(String[] args) {
+        Aluno2 felipe = new Aluno2();
+
+        felipe.setNome("Felipe");
+        felipe.setIdade(12);
+        felipe.setSexo("masculino");
+
+        System.out.println("O aluno" + felipe.getNome() + " tem " + felipe.getIdade() + " anos " + " do sexo " + felipe.getSexo());
+    }
+}
